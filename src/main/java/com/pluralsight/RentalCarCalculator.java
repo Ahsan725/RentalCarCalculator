ac83d8972e5fe74d7941a16f56d101d200b415f1
Ahsan <sbaseer25@gmail.com>
2025-09-30 11:55:42 -0400

    Completed!

--- src/main/java/com/pluralsight/RentalCarCalculator.java
+++ src/main/java/com/pluralsight/RentalCarCalculator.java
@@ -12,19 +12,47 @@ public class RentalCarCalculator {
         int age;
         double basicCarRental = 29.99;
         double optionsCost;
-        double underageDriverSurcharge;
-        double total;
+        double underageDriverSurcharge = 1.3;
+        double total = 0;
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("What is the data you need the car?");
+        System.out.println("What is the date you need the car?");
         pickupDate = scanner.nextLine();
 
         System.out.println("How many days do you need the car for?");
-        scanner.nextLine();
+//        scanner.nextLine();
         numDays = scanner.nextInt();
 
-        System.out.println("Would you like an electronic toll tag?");
+        System.out.println("Would you like an electronic toll tag? Enter True or False");
+        electronicTag = scanner.nextBoolean();
+        if (electronicTag){
+            total += 3.95 * numDays;
+        }
 
+        System.out.println("Would you like to add GPS?");
+        GPS = scanner.nextBoolean();
+        if (GPS){
+            total += 2.95 * numDays;
+        }
+
+        System.out.println("Would you like roadside assistance? Enter True or False");
+        roadsideAssistance = scanner.nextBoolean();
+        if (roadsideAssistance){
+            total += 3.95 * numDays;
+        }
+
+        System.out.println("What is your age?");
+        age = scanner.nextInt();
+        scanner.nextLine();
+
+        //calculations
+        if (age < 25){
+            total += (basicCarRental * numDays) * underageDriverSurcharge;
+        }else{
+            total += basicCarRental;
+        }
+
+        System.out.println("Success! You have booked the car for " + pickupDate + " for " + numDays + " days. Your total is: $" + total);
 
     }
 }
